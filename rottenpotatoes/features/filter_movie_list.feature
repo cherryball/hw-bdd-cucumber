93cd1a140f5c9806e99d0aff57c9bf6ed9688884
cherryball <greencherryball@gmail.com>
2015-12-02 06:57:59 +0000

    fitler-step

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -23,10 +23,23 @@ Background: movies have been added to database
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
+  When I check the following ratings: PG, R
+  Then the ratings_PG,ratings_R checkbox should be checked
 
+  # enter step(s) to uncheck all other checkboxes
+  When I uncheck the following ratings: G, PG-13
+  Then the ratings_G, ratings_PG-13, ratings_PG checkbox should be unchecked
+  
+  # enter step to "submit" the search form on the homepage
+  When I press "Refresh"
+  
+  # enter step(s) to ensure that PG and R movies are visible
+  Then I should see ratgins_PG, ratings_R 
+
+  # enter step(s) to ensure that other movies are not visible
+  Then I should not see ratings_G, ratings_PG-13
+  
 Scenario: all ratings selected
   # see assignment
+  When I check the following ratings: PG, R, G, PG-13
+  Then I should see all the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,11 +1,14 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
+  @movies = []
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    m = Movie.create(title: movie['title'], rating: movie['rating'], release_date: movie['release_date'])
+    @movies.push(m)
   end
-  fail "Unimplemented"
+#  fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -25,10 +28,42 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+#  fail "Unimplemented"
+  rating_list.split(',').each do |r|
+    r.strip!
+    uncheck ? uncheck("ratings_#{r}") : check("ratings_#{r}")
+  end
+#  '/movies'
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  # assert page.find("#movies").rows == 11
+  page.all('table#movies tr').count.should == 11
 end
+
+Then(/^I should see ratgins_PG, ratings_R$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should not see ratings_G, ratings_PG\-(\d+)$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the "([^"]*)" checkbox(?: within (.*))? should be checked$/ do |label, parent|
+  with_scope(parent) do
+    field_checked = find_field(label)['checked']
+    if field_checked.respond_to? :should
+      field_checked.should be_true
+    else
+      assert field_checked
+    end
+  end
+end
+
+Then /^the (.*) checkbox should be (un)?checked$/ do |id, uncheck|
+  id.split(",").each do |i| 
+    find_field(i)[:value].should eq "1" unless uncheck
+  end
+end
+
